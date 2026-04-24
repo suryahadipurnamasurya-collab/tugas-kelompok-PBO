28fe857e7748d7bbb50b190106de72636a1aa1ea
suryahadipurnamasurya-collab <suryahadipurnamasurya@kamu.com>
2026-04-24 18:19:42 +0700

    pembenaran nama kalian guys

--- UTS PBO/src/GenisysGUI.java
+++ UTS PBO/src/GenisysGUI.java
@@ -155,9 +155,7 @@ public class GenisysGUI extends JFrame {
         identityLog.setForeground(new Color(0, 255, 0)); 
         identityLog.setText("SYSTEM BOOTING...\n" +
                           "Lab Pusat UNESA - Divisi Genetik\n" +
-                          "Nama Pembuat : Surya, Ray, Muti, Fauzan, nadip, saka, salsa, maman\n" +
-                          "NIM          : 25051204402\n" +
-                          "NIM          : 25051204388\n" +
+                          "dibuat oleh  : Surya, Ray, Mutiara, Fauzan, Nadhif, Sakarina, Salsa, Mandriva\n" +
                           "Status       : DATABASE ONLINE. READY FOR INPUT.");
         panelHeader.add(identityLog, BorderLayout.WEST);
 
